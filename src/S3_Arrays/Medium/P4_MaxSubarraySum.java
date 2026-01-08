42c86583f397ba4bb8a371fb4f5a27c6077a1763
str-cd <vikasvsahu121@gmail.com>
2026-01-08 22:48:53 +0530

    aRRAY MEDIUM p4-p5

---
+++ src/S3_Arrays/Medium/P4_MaxSubarraySum.java
@@ -0,0 +1,57 @@
+package S3_Arrays.Medium;
+
+public class P4_MaxSubarraySum {
+    public static void main(String[] args) {
+
+       int [] arr = {2, 3, 5, -2, 7, -4};
+
+        System.out.println(maxSubArray(arr));
+
+    }
+
+    public static int maxSubArray(int[] nums) {
+        // brute force/ better solution
+        /*
+        int res =Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+
+                res = Math.max(res,sum);
+
+            }
+
+        }
+        return res;
+        */
+
+        // optimal
+
+        int max = Integer.MIN_VALUE;
+        int sum=0;
+
+        int start =-1;
+        int end = -1;
+        for(int i=0;i<nums.length;i++){
+            if(sum==0){
+                start=i;
+            }
+            sum+=nums[i];
+
+            if(sum>max){
+                end=i;
+                max=sum;
+            }
+
+            if(sum<0){
+                sum=0;
+            }
+        }
+
+        System.out.println(start + " " + end);
+        return max;
+
+    }
+}
---
+++ src/S3_Arrays/Medium/P5_PrintsubarrayWithMaxSum.java
@@ -0,0 +1,41 @@
+package S3_Arrays.Medium;
+
+public class P5_PrintsubarrayWithMaxSum {
+    public static void main(String[] args) {
+        int [] arr = {2, 3, 5, -2, 7, -4};
+        int [] res = maxSubArray(arr);
+
+        for(int i:res){
+            System.out.print(i+" ");
+        }
+
+    }
+
+    public static int [] maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum=0;
+
+        int start =-1;
+        int end = -1;
+        for(int i=0;i<nums.length;i++){
+            if(sum==0){
+                start=i;
+            }
+            sum+=nums[i];
+
+            if(sum>max){
+                end=i;
+                max=sum;
+            }
+
+            if(sum<0){
+                sum=0;
+            }
+        }
+
+
+
+        return max;
+
+    }
+}
